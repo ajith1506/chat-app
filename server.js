e00bf95a58b0c49e9903fc527605ac585dd3a43b
ajith1506 <ajithkumararun1111@gmail.com>
2024-10-25 11:24:34 +0530

    changes

--- server.js
+++ server.js
@@ -80,10 +80,10 @@ server.listen(PORT, () => {
 });
 
 // --------------------------deployment------------------------------
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 // --------------------------deployment------------------------------
