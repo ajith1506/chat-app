1eeb452ea41529afb75c3b4d54204c4ff9ccaa90
ajith1506 <ajithkumararun1111@gmail.com>
2024-10-25 11:18:41 +0530

    changes

--- server.js
+++ server.js
@@ -74,13 +74,13 @@ io.on("connection", (socket) => {
 });
 // --------------------------deployment------------------------------
 
-const __dirname1 = path.resolve();
+const __dirname1 = path.resolve(__dirname); // This ensures you're getting the directory of the current file
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/FrontEnd/build")));
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "FrontEnd", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
