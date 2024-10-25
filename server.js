6625afec030d59e11cfb1e16ac7fe308e6cfee9e
ajith1506 <ajithkumararun1111@gmail.com>
2024-10-25 11:23:47 +0530

    changes

--- server.js
+++ server.js
@@ -72,26 +72,18 @@ io.on("connection", (socket) => {
     });
   });
 });
-// --------------------------deployment------------------------------
-
-const __dirname1 = path.resolve(__dirname); // This ensures you're getting the directory of the current file
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
-
-// --------------------------deployment------------------------------
 
 const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => {
   console.log("Server Work in ", PORT);
 });
+
+// --------------------------deployment------------------------------
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
+// --------------------------deployment------------------------------
